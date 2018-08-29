30e3a85cc9d9f6cceacbe78e5a6d052e98dd6742
hodongman <jhd9206@gmail.com>
2018-08-30 01:13:42 +0900

    akka actor fibonacci

--- src/main/scala/com/hodong/chapter1/BehaviorAndState.scala
+++ src/main/scala/com/hodong/chapter1/BehaviorAndState.scala
@@ -5,8 +5,13 @@ import akka.actor.{Props, Actor, ActorSystem}
 object BehaviorAndState extends App {
     val actorSystem = ActorSystem("HelloAkka")
 
-    val actor = actorSystem.actorOf(Props[SummingActor], "ActorName")
-    println(actor.path)
+    val actor = actorSystem.actorOf(Props(classOf[SummingActorWithConstructor], 10), "ActorName")
+    
+    while (true)
+    {
+      Thread.sleep(3000)
+      actor ! 1
+    }
 }
 
 class SummingActor extends Actor {
@@ -21,4 +26,17 @@ class SummingActor extends Actor {
     case _ => println("I don't know what are you talking about")
 
   }
+}
+
+class SummingActorWithConstructor(initalSum: Int) extends Actor {
+
+    var sum = 0
+
+    override def receive: Receive = {
+    
+        case x: Int => sum = sum + x
+        println(s"my state as sum is $sum")
+        // receives default message
+        case _ => println("I don't know what are you talking about")
+    }
 }
